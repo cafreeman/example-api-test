a0b198950fb949222cba340c329904013eece962
Chris Freeman <cfreeman.code@gmail.com>
2020-01-21 15:37:02 -0700

    star wars test example

---
+++ tests/people.test.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+const Luke = require('../fixtures/lukeSkywalker');
+
+const SWAPI = 'http://swapi.co/api';
+
+// make axios fill in the first part of the URL for us
+axios.defaults.baseURL = SWAPI;
+
+describe('/people', () => {
+  let response, body;
+
+  beforeAll(async () => {
+    response = await axios.get('/people');
+    body = response.data;
+  });
+
+  test('the response has a count property', () => {
+    expect(body).toHaveProperty('count');
+  });
+
+  test('returns a results key that contains an array', () => {
+    expect(body).toHaveProperty('results');
+    expect(body.results).toBeInstanceOf(Array);
+  });
+
+  test('the results array contains a person object', () => {
+    expect(body.results[0]).toEqual(Luke);
+  });
+});
+
+describe('/people/:id', () => {
+  test('looking up luke by id', async () => {
+    const response = await axios.get('/people/1');
+
+    expect(response.data).toEqual(Luke)
+  });
+
+  test('looking up luke by name', async () => {
+    const response = await axios.get('/people/1?name="Luke Skywalker"');
+
+    expect(response.data).toEqual(Luke)
+  });
+});
